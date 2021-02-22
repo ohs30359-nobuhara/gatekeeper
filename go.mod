24ddb046a9e9c4f3ce2513e6007a3b1d74e0551c
信原 有志 <ynobuhar@yahoo-corp.jp>
2021-02-22 22:12:13 +0900

    prometheus exporter

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-redis/redis/v8 v8.4.4
 	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/prometheus/client_golang v1.9.0
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
 )
--- proxy.go
+++ proxy.go
@@ -4,12 +4,15 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/my/repo/cache"
+	"github.com/my/repo/metricsExporter"
 	"github.com/my/repo/rateLimit"
 	"github.com/my/repo/util"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 	"unsafe"
 )
@@ -25,10 +28,13 @@ type ProxyConfig struct {
 	host string
 }
 
-var cache *customCache.Redis
-var config *util.Config
-var rateLimiter *rateLimit.RateLimit
-var proxyConfig *ProxyConfig
+var (
+	cache *customCache.Redis
+	config *util.Config
+	rateLimiter *rateLimit.RateLimit
+	proxyConfig *ProxyConfig
+	exporter *metricsExporter.CacheMetrics
+)
 
 // proxyHandler handler
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,17 +48,22 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 
 	key := r.RequestURI
 
+	// delete path "/proxy"
+	r.RequestURI = strings.Replace(r.RequestURI, "/proxy", "", 1)
+
 	cr := cloneRequest(r)
 
 	if r.Method == "GET" {
 		// cache read only get request
 		if v, hit := cache.Get(key); hit {
+			exporter.Count(r.RequestURI, "true")
 			writeBody(w, *(*[]byte)(unsafe.Pointer(&v)))
 			return
 		}
 
 		data = curl(cr)
-		cache.Set(key, *(*string)(unsafe.Pointer(&data.Body)), time.Second * 1)
+		cache.Set(key, *(*string)(unsafe.Pointer(&data.Body)), time.Second * 10)
+		exporter.Count(r.RequestURI, "false")
 	} else {
 		data = curl(cr)
 	}
@@ -62,6 +73,10 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	writeBody(w, data.Body)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	writeBody(w, []byte(http.StatusText(http.StatusOK)))
 }
 
 // overwrite client request host
@@ -164,13 +179,19 @@ func main() {
 	p := ":" + config.Server.Port
 	log.Println("server listen localhost" + p)
 
+	exporter = metricsExporter.Init()
+
 	// setup proxy config
 	proxyConfig = &ProxyConfig{
 		schema: config.Proxy.Target.Schema,
 		host: config.Proxy.Target.Host + ":" + config.Proxy.Target.Port,
 	}
 
-	if err := http.ListenAndServe(p, http.HandlerFunc(proxyHandler)); err != nil {
+	http.HandleFunc("/proxy/", proxyHandler)
+	http.HandleFunc("/", health)
+	http.Handle("/metrics", promhttp.Handler())
+
+	if err := http.ListenAndServe(p, nil); err != nil {
 		panic(err.Error())
 	}
 }
